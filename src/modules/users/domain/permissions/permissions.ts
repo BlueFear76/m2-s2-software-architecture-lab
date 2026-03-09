3c5a70fe847035f2348764cadbf7927e701a1c4d
BlueFear <bluefear1976@gmail.com>
2026-03-09 19:34:39 +0100

    feat(users) : creation des permissions utilisateurs liées au tags

--- src/modules/users/domain/permissions/permissions.ts
+++ src/modules/users/domain/permissions/permissions.ts
@@ -1,10 +1,14 @@
 import { UserRole } from '../entities/user.entity';
 import { PostPermissions } from './post-permissions';
+import { TagPermissions } from './tag-permissions';
 
 export class Permissions {
   public readonly posts: PostPermissions;
+  public readonly tags: TagPermissions;
+
 
   constructor(userId: string, role: UserRole) {
     this.posts = new PostPermissions(userId, role);
+    this.tags = new TagPermissions(userId, role);
   }
 }
---
+++ src/modules/users/domain/permissions/tag-permissions.ts
@@ -0,0 +1,20 @@
+import { UserRole } from '../entities/user.entity';
+
+export class TagPermissions {
+  constructor(
+    private readonly userId: string,
+    private readonly role: UserRole,
+  ) {}
+
+  public canCreate(): boolean {
+    return this.role === 'admin';
+  }
+
+  public canUpdateName(): boolean {
+    return this.role === 'admin';
+  }
+
+  public canDeleteTag(): boolean {
+    return this.role === 'admin';
+  }
+}
